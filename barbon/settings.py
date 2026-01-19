bf8afa313a768a8c54d687f929536667fbe6ba42
Enrique Cucalon <enriquecucalon48@gmail.com>
2026-01-19 18:03:29 -0500

    Switch database to Postgres

--- barbon/settings.py
+++ barbon/settings.py
@@ -82,10 +82,11 @@ WSGI_APPLICATION = 'barbon.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
