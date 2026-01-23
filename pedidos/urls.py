bb0d4c59bd788b420c1bb69f2c9f96b0a2330f3f
Enrique Cucalon <enriquecucalon48@gmail.com>
2026-01-23 16:50:08 -0500

    Fix url names to resolve 500 error

--- pedidos/urls.py
+++ pedidos/urls.py
@@ -5,11 +5,11 @@ from .views import (
 )
 
 urlpatterns = [
-    path("", inicio),
-    path("menu/", menu),
+    path("", inicio, name="inicio"),
+    path("menu/", menu, name="menu"),
 
-    path("api/pedido/<int:pedido_id>/", api_pedido),
+    path("api/pedido/<int:pedido_id>/", api_pedido, name="api_pedido"),
 
-    path("bot/confirmar/<int:pedido_id>/", confirmar_pedido_bot),
-    path("bot/guardar-chat/<int:pedido_id>/", guardar_chat_id),
+    path("bot/confirmar/<int:pedido_id>/", confirmar_pedido_bot, name="confirmar_pedido_bot"),
+    path("bot/guardar-chat/<int:pedido_id>/", guardar_chat_id, name="guardar_chat_id"),
 ]
